c7c7a1b9ec26396f1ee867a1d5fe70622a6a27ae
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-10-20 09:32:33 +0200

    proyecto imágenes

--- Pizzeria/Pizzeria/MainWindow.xaml.cs
+++ Pizzeria/Pizzeria/MainWindow.xaml.cs
@@ -45,32 +45,39 @@ namespace Pizzeria
         private void desmarcar(UIElementCollection elementos)
         {
             foreach (UIElement element in elementos)
-            {
-                if (element is ToggleButton toggleButton)
-                {
-
+                if (element is ToggleButton toggleButton)             
                     if (toggleButton.IsChecked == true)
-                    {
                         toggleButton.IsChecked = false;
-                    }
-                }
-            }
         }
 
         private void procesarElementos(UIElementCollection elementos)
         {
             foreach (UIElement element in elementos)
-            {
                 if (element is ToggleButton toggleButton)
-                {
-                    
                     if (toggleButton.IsChecked == true)
-                    {
                         miListado.Items.Add(toggleButton.Content);
-                    }
-                }
-            }
         }
+
+        private void rbCola_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarImagenBebida("cola");
+        }
+
+        private void rb7up_Checked(object sender, RoutedEventArgs e)
+        {
+
+            cambiarImagenBebida("7up");
+        }
+
+        private void cambiarImagenBebida(string nombreImagen)
+        {
+            string rutaImagen = $"/{nombreImagen}.png";
+            
+            BitmapImage nuevaImagen = new BitmapImage(new Uri(rutaImagen, UriKind.Relative));
+            
+            imgBebida.Source = nuevaImagen;
+        }
+
 
     }
 }
