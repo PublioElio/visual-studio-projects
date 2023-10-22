15d9dc94501fed897b5d9fc1828f79a8213955a8
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-10-22 21:15:01 +0200

    proyecto pizzeria con imagenes

--- Pizzeria/Pizzeria/MainWindow.xaml.cs
+++ Pizzeria/Pizzeria/MainWindow.xaml.cs
@@ -27,6 +27,8 @@ namespace Pizzeria
             InitializeComponent();
         }
 
+        // Aquí debajo empiezan los botones y sus eventos
+
         private void pedir_Click(object sender, RoutedEventArgs e)
         {
             procesarElementos(spBebidas.Children);
@@ -45,40 +47,82 @@ namespace Pizzeria
         private void desmarcar(UIElementCollection elementos)
         {
             foreach (UIElement element in elementos)
-                if (element is ToggleButton toggleButton)             
-                    if (toggleButton.IsChecked == true)
-                        toggleButton.IsChecked = false;
+                if (element is ToggleButton toggleButton && toggleButton.IsChecked == true)
+                    toggleButton.IsChecked = false;
+
+            limpiarImagenesEnGrid(ventanaPrincipal);
+        }
+
+        private void limpiarImagenesEnGrid(Grid grid)
+        {
+            foreach (UIElement element in grid.Children)
+                if (element is Image image && image.Name != "mario")
+                    image.Source = null;
+            
         }
 
         private void procesarElementos(UIElementCollection elementos)
         {
             foreach (UIElement element in elementos)
-                if (element is ToggleButton toggleButton)
-                    if (toggleButton.IsChecked == true)
-                        miListado.Items.Add(toggleButton.Content);
+                if (element is ToggleButton toggleButton && toggleButton.IsChecked == true)
+                    miListado.Items.Add(toggleButton.Content);
         }
+
+        // Aquí los RadioButton de las bebidas y su método para cambiar la imagen
 
         private void rbCola_Checked(object sender, RoutedEventArgs e)
         {
-            cambiarImagenBebida("cola");
+            cambiarImgBebida("cola");
         }
 
         private void rb7up_Checked(object sender, RoutedEventArgs e)
         {
+            cambiarImgBebida("sevenup");
 
-            cambiarImagenBebida("7up");
         }
 
-        private void cambiarImagenBebida(string nombreImagen)
+        private void rbFanta_Checked(object sender, RoutedEventArgs e)
         {
-            string rutaImagen = $"/{nombreImagen}.png";
-            
-            BitmapImage nuevaImagen = new BitmapImage(new Uri(rutaImagen, UriKind.Relative));
-            
-            imgBebida.Source = nuevaImagen;
+            cambiarImgBebida("fanta");
+        }
+
+        private void rbAgua_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarImgBebida("agua");
+        }
+
+        private void cambiarImgBebida(string imageSrc) {
+
+            Uri uri = new Uri($"/{imageSrc}.png", UriKind.Relative);
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            imgBebida.Source = bitmapImage;
 
         }
 
+        // Aquí los RadioButton de la masa y su método para cambiar la imagen
 
+        private void rbQueso_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarImgMasa("queso");
+        }
+
+        private void rbFina_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarImgMasa("fina");
+        }
+
+        private void rbClasica_Checked(object sender, RoutedEventArgs e)
+        {
+            cambiarImgMasa("clasica");
+        }
+
+        private void cambiarImgMasa(string imageSrc)
+        {
+
+            Uri uri = new Uri($"/{imageSrc}.png", UriKind.Relative);
+            BitmapImage bitmapImage = new BitmapImage(uri);
+            imgMasa.Source = bitmapImage;
+
+        }
     }
 }
