3d64de4ded068d844c62406e54fed34ac3fc9ff5
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-10-24 21:58:17 +0200

    ACTUALIZACION TAREA

--- 02_poject/MainWindow.xaml.cs
+++ 02_poject/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,32 +21,57 @@ namespace _02_poject
     /// </summary>
     public partial class MainWindow : Window
     {
-        List<User> userList = new List<User> { };
-
-        public MainWindow()
-        {
-            InitializeComponent();
-            dataGridUsers.ItemsSource = userList;
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            
-            User user = new User();
-            user.name = textBoxUserName.Text;
-            user.surname = textBoxSurname.Text;
-            user.adress = textBoxAdress.Text;
-            user.phone = textBoxPhone.Text;
-            userList.Add(user);
-            
-        }
-
         private class User
         {
             public string name { get; set; }
             public string surname { get; set; }
             public string adress { get; set; }
             public string phone { get; set; }
+        }
+
+        // I've used ObservableCollection because it's a data structure that allows notifying data changes (it implements the INotifyPropertyChanged interface)
+        ObservableCollection<User> userList = new ObservableCollection<User> { };
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            dataGridUsers.ItemsSource = userList;       
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string name, surname, adress, phone;
+            iniztalizeUser(out name, out surname, out adress, out phone);
+
+            if (isFilled(name, surname, adress, phone))
+            {
+                userList.Add(new User { name = name, surname = surname, adress = adress, phone = phone });
+                clearTexBoxes();
+            }
+            else
+                MessageBox.Show("No es posible dejar campos en blanco.");
+
+        }
+
+        private void clearTexBoxes()
+        {
+            textBoxUserName.Clear();
+            textBoxSurname.Clear();
+            textBoxAdress.Clear();
+            textBoxPhone.Clear();
+        }
+
+        private static bool isFilled(string name, string surname, string adress, string phone)
+        {
+            return !string.IsNullOrEmpty(name) && !string.IsNullOrEmpty(surname) && !string.IsNullOrEmpty(adress) && !string.IsNullOrEmpty(phone);
+        }
+
+        private void iniztalizeUser(out string name, out string surname, out string adress, out string phone)
+        {
+            name = textBoxUserName.Text;
+            surname = textBoxSurname.Text;
+            adress = textBoxAdress.Text;
+            phone = textBoxPhone.Text;
         }
     }
 }
