b65d0a1dec20a47ab4208718e99845502ca9634a
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-08 13:23:02 +0100

    revertido a toggleButton

--- 02_poject/obj/Debug/net6.0-windows/MainWindow.g.i.cs
+++ 02_poject/obj/Debug/net6.0-windows/MainWindow.g.i.cs
@@ -325,7 +325,7 @@ namespace _02_poject {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/02_poject;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/02_poject;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
