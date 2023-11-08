823b93f29a99f1ab60d100af679746240e779437
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2023-11-08 14:45:32 +0100

    actualizacion

--- calculo_salario/MainWindow.xaml.cs
+++ calculo_salario/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace calculo_salario
         {
             InitializeComponent();
         }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
     }
 }
