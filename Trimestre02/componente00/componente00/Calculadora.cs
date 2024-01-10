0677ee7d133ad1d34374122795b93b37fe7f4e08
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-10 14:43:25 +0100

    cambios

---
+++ Trimestre02/componente00/componente00/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace componente00
+{
+    public class Calculadora
+    {
+        public int suma(int num1, int num2) 
+        { 
+            return num1 + num2;
+        }
+        public int resta(int num1, int num2) 
+        { 
+            return num1 + num2;
+        }
+        public int multiplicacion(int num1, int num2) 
+        { 
+            return num1 * num2;
+        }
+    }
+}
