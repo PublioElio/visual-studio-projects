f141756fe6cad2deb5be5af25ef2baa89ec2d7dd
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-11 09:42:00 +0100

    actualizado

--- Trimestre02/componente00/componente00/obj/Debug/net8.0/componente00.AssemblyInfo.cs
+++ Trimestre02/componente00/componente00/obj/Debug/net8.0/componente00.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("componente00")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd6b4d437fb88d548a760bb865c700f437b12e7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("componente00")]
 [assembly: System.Reflection.AssemblyTitleAttribute("componente00")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
