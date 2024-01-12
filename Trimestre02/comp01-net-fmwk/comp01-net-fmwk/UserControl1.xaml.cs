1755595d7474782130cf6e7dfdbfca7a82892ebc
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-12 08:30:31 +0100

    nuevo proyecto

---
+++ Trimestre02/comp01-net-fmwk/comp01-net-fmwk/UserControl1.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace comp01_net_fmwk
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+        public string Label
+        {
+            get => miLabel.Content.ToString();
+            set => miLabel.Content = value;
+        }
+        public string TextBox
+        {
+            get => miTextBox.Text.ToString();
+            set => miTextBox.Text = value;
+        }
+
+    }
+}
