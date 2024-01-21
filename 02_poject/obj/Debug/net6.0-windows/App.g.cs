99fa9c0a07dd3b95ecc81ade535801edb9077fd1
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-21 17:24:18 +0100

    cambios en la app

--- 02_poject/obj/Debug/net6.0-windows/App.g.cs
+++ 02_poject/obj/Debug/net6.0-windows/App.g.cs
@@ -45,7 +45,7 @@ namespace _02_poject {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public void InitializeComponent() {
             
             #line 5 "..\..\..\App.xaml"
@@ -60,7 +60,7 @@ namespace _02_poject {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "7.0.12.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "8.0.1.0")]
         public static void Main() {
             _02_poject.App app = new _02_poject.App();
             app.InitializeComponent();
