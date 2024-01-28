fac6b470b285f030dbd4015564669686d69f6e8e
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-28 10:51:18 +0100

    proyecto reloj

--- Trimestre02/comp03-net-fmwk/comp03-net-fmwk/comp03-net-fmwk/UserControl1.xaml.cs
+++ Trimestre02/comp03-net-fmwk/comp03-net-fmwk/comp03-net-fmwk/UserControl1.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -21,11 +22,41 @@ namespace comp03_net_fmwk
     /// </summary>
     public partial class UserControl1 : UserControl
     {
+        private int hora;
+        private int minutos;
         public UserControl1()
         {
             InitializeComponent();
+            MostrarTiempo(hora, minutos);
         }
 
+        private void MostrarTiempo(int hora, int minutos) {
+            if (hora < 10)
+            {
+                PintarHora("0" + hora);
+            }
+            else if (hora > 9 && hora < 24)
+            {
+                PintarHora(hora + "");
+            }
+            else {
+                PintarHora("00");
+            }
 
+        }
+
+        private void PintarHora(string hora) {
+
+            if (!string.IsNullOrEmpty(hora))
+            {
+                char decenas = hora[0];
+                char unidades = hora[1];
+            }
+        }
+
+        private void PintarDigito(char digito, int posicion) {
+
+            
+        }
     }
 }
