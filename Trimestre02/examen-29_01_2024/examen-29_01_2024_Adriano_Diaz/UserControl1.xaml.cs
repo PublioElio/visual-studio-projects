69693c3d8c6b119366fe0bd9b05690bf350a9144
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-01-29 10:33:45 +0100

    examen tema 04

---
+++ Trimestre02/examen-29_01_2024/examen-29_01_2024_Adriano_Diaz/UserControl1.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace examen_29_01_2024_Adriano_Diaz
+{
+    /// <summary>
+    /// Lógica de interacción para UserControl1.xaml
+    /// </summary>
+    public partial class UserControl1 : UserControl
+    {
+        private int ListBoxMaxItems = 0;
+        public UserControl1()
+        {
+            InitializeComponent();
+        }
+
+        [Description("Número máximo de elementos del List Box"), Category("Mi categoria"), DisplayName("Número máximo de elementos")]
+        public int ListBoxMaxElements
+        {
+            get => ListBoxMaxItems;
+            set => ListBoxMaxItems = value;
+        }
+
+        [Description("Número máximo de caracteres del Text Box"), Category("Mi categoria"), DisplayName("Número máximo de caracteres del Text Box")]
+        public int TextboxMaxlenght
+        {
+            get => miTextBox.MaxLength;
+            set => miTextBox.MaxLength = value;
+        }
+
+        // [Description("Color de fondo del Slider"), Category("Mi categoria"), DisplayName("Color de fondo del Slider")]
+
+        private void PulsarEnter(object sender, KeyEventArgs e)
+        {
+            if ((e.Key == Key.Enter) && (miListBox.Items.Count < ListBoxMaxItems))
+            {
+                miListBox.Items.Add(miTextBox.Text);
+                miTextBox.Text = string.Empty;
+                ComprobarCapacidadListBox();
+            }
+        }
+
+        // 3. Cada vez que los elementos del listbox cambian se actualiza el slider,
+        // cuyo máximo coincidirá con el número máximo de elementos del listbox.
+        // El usuario no puede modificar el slider manualmente. (1 pto.)
+
+        // 4. Cuando el listbox llega al límite de su capacidad,
+        // el color del textbox se cambia a rojo y ya no se pueden
+        // seguir introduciendo datos. (1 pto.)
+
+
+
+        private void ComprobarCapacidadListBox() {
+            if (miListBox.Items.Count == ListBoxMaxItems)
+            {
+                miTextBox.Background = Color.FromRgb(255, 255, 255);
+            }
+            else { 
+            
+            }
+        }
+    }
+}
