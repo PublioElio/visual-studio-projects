12fcb584c37405a673be3668fa0648651ff98a27
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-02-01 20:55:20 +0100

    ejercicio marco imagen, leyenda y open dialog

---
+++ Trimestre02/comp04-net-fmwk/comp04-net-fmwk/comp04-net-fmwk/MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.WebRequestMethods;
+
+namespace comp04_net_fmwk
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files | *.jpg; *.png;";
+            if (openFileDialog.ShowDialog() == true) {
+                compImgLeyenda.RecursoImagen = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Absolute));
+                string[] strings = openFileDialog.FileName.Split('\\');
+                compImgLeyenda.TextoLeyenda = System.IO.Path.GetFileNameWithoutExtension(strings[strings.Length - 1]);
+            }
+                
+        }
+
+
+
+    }
+}
---
+++ Trimestre02/comp04-net-fmwk/comp04-net-fmwk/comp04-net-fmwk/imgLeyenda.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace comp04_net_fmwk
+{
+    /// <summary>
+    /// Lógica de interacción para imgLeyenda.xaml
+    /// </summary>
+    public partial class imgLeyenda : UserControl
+    {
+        public imgLeyenda()
+        {
+            InitializeComponent();
+        }
+
+        [Description("Texto de la leyenda"), Category("Mi categoria"), DisplayName("Leyenda")]
+        public string TextoLeyenda { get => TextBoxLeyenda.Text; set => TextBoxLeyenda.Text = value; }
+
+        [Description("Contenido imagen"), Category("Mi categoria"), DisplayName("Contenido imagen")]
+        public ImageSource RecursoImagen
+        {
+            get => ImgCentral.Source;
+            set
+            {
+                if (value != null)
+                {
+                    ImgCentral.Source = value;
+                    BorderImg.BorderBrush = new SolidColorBrush(Colors.Transparent);
+                }
+            }
+        }
+
+        private void Img_MouseWeel(object sender, MouseWheelEventArgs e)
+        {
+
+        }
+    }
+}
