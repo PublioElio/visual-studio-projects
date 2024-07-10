6a99ffb11da885a5195ed68ecd314f788b1f40b1
Adriano Díaz Benítez <adrianodiazb@gmail.com>
2024-07-10 10:54:59 +0200

    changed security issues

---
+++ Trimestre02/Unidad_5/pruebas_casa/informe_01/informe_01/informe_01/Comprador.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace informe_01
+{
+    internal class Comprador
+    {
+        public int Idcomprador { get; set; }
+        public string Nombre { get; set; }
+        public string Telefono { get; set; }
+    }
+}
